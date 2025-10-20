f3ca3d76d0e16b82d400ea2ec339d14a8a39b3e0
ckalgara <chandhukalagara@gmail.com>
2025-10-20 18:21:36 -0500

    chore: spring init

---
+++ src/main/java/com/ckalagara/group_b_shop/GroupBShopApplication.java
@@ -0,0 +1,13 @@
+package com.ckalagara.group_b_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupBShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupBShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ckalagara/group_b_shop/GroupBShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ckalagara.group_b_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupBShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
